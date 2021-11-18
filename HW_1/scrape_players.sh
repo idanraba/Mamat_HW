3b61a54f575c0e26dcd914ec8b582a5341b8ee4d
idanraba <idan9raba@gmail.com>
2021-11-18 16:50:12 +0200

    add comment in file HW 1

--- HW_1/scrape_players.sh
+++ HW_1/scrape_players.sh
@@ -1,4 +1,14 @@
 #!/bin/bash                                       
+#line 1: browses and export the information from the web to "players" file
+#line 2-3: took the specifc request pattern to "urls.txt" file
+#line 4-5: counts the number of players and poot them in "Results" file
+#line 6: make the link legal
+#line 7-14: every line, browers from the link in overview file, and
+#           search for midfielder word, and count them, and save them in
+#           Results.csv file
+#line 15-16: delete the files
+#line 17: print the Results.csv file
+
 wget https://www.premierleague.com/players &> /dev/null  
 
 grep -oP "/players/[0-9]+/[a-zA-Z-]+/overview" players > urls.txt
